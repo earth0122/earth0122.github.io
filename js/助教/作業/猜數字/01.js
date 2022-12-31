94471fcba6b72619330386489cdc243709a704c1
Earth <earth850122@gmail.com>
2023-01-01 01:28:16 +0800

    okkk

--- js/助教/作業/猜數字/01.js
+++ js/助教/作業/猜數字/01.js
@@ -1,10 +1,10 @@
-1; //取得DOM元素和需要用到的變數
+//取得DOM元素和需要用到的變數
 //抓到DOM input-num
 let inputNum = document.querySelector("#input-num");
 console.log(inputNum);
 //抓到DOM btn-num
-let allBtn = document.querySelectorAll(".btn-num");
-console.log(allBtn);
+// let allBtn = document.querySelectorAll(".btn-num");
+// console.log(allBtn);
 //抓到DOM btn-reset
 let resetBtn = document.querySelector(".btn-reset");
 console.log(resetBtn);
@@ -20,23 +20,47 @@ let startBtn = document.querySelector(".btn-start");
 let inputNumValue;
 //猜的數字
 let guessNum;
-//按鈕輸入的數字 存進inputNum
-allBtn.forEach((item) => {
-  item.addEventListener("click", function () {
-    inputNum.value += item.innerHTML;
-  });
+//猜數字的答案
+let answer = getRandomInt();
+//案答案按鈕要彈出答案
+answerBtn.addEventListener("click", function () {
+  alert(answer);
 });
-//重新開始遊戲
-// resetBtn.addEventListener("click", function () {
-//   inputNum.value("");
+//按鈕輸入的數字 存進inputNum
+// allBtn.forEach((item) => {
+//   item.addEventListener("click", function () {
+//     inputNum.value += item.innerHTML;
+//   });
 // });
 
 //鎖住按鈕
+window.onload = function () {
+  // allBtn.disabled = true;
+  answerBtn.disabled = true;
+  guessBtn.disabled = true;
+  resetBtn.disabled = true;
+};
+//案開始
+startBtn.addEventListener("click", function () {
+  answerBtn.disabled = false;
+  guessBtn.disabled = false;
+  resetBtn.disabled = false;
+});
 
 //猜數字
 
-//1.產生一組4位數 而且不重複的數字
+//1產生一組4位數
+function getRandomInt(min, max) {
+  min = Math.ceil(1000);
+  max = Math.floor(10000);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
 //2.將輸入的數字判斷是否正確&位置是否正確
 
-//
+//3.重新開始遊戲
+resetBtn.addEventListener("click", function () {
+  answer = getRandomInt();
+  inputNum.value("");
+  // allBtn.disabled = true;
+});
