a7132f0d56404c01757db5daeda4ba0845c4138c
Earth <earth850122@gmail.com>
2023-01-06 01:49:05 +0800

    okk

---
+++ js/lesson/Map/map_bascic.js
@@ -0,0 +1,5 @@
+var Cars = ["BMW", "Benz", "Audi", "Lexus"];
+var Prices = [280, 320, 250, 210];
+// map()⽅法是對每⼀個陣列元素作運算，並將結果回傳⾄新陣列
+var PricesWithTax = Prices.map((p) => p * 1.05);
+console.log(PricesWithTax);
