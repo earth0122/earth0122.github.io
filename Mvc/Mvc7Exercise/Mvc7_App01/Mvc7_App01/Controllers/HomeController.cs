d0b1788ae5df5eb8caceda3da50c244c4a0bdddb
earth <earth850122@gmail.com>
2023-02-11 14:51:52 +0800

    111

---
+++ Mvc/Mvc7Exercise/Mvc7_App01/Mvc7_App01/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc7_App01.Models;
+using System.Diagnostics;
+
+namespace Mvc7_App01.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult Index() 
+        //{
+        //    Product product = new Product
+        //    {
+        //        Id = 1,
+        //        Name = "SSD 512GB",
+        //        Price = 2500
+        //    };
+        //    return View(product);
+        //}
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string GetName()
+        {
+            return "Mary";
+        }
+        
+        public IActionResult Index()
+        {
+            return View("Privacy");
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mvc/Mvc7Exercise/Mvc7_App01/Mvc7_App01/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc7_App01.Models;
+
+namespace Mvc7_App01.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //public ProductsController()
+        //{
+        //    this.Response
+        //}
+        public IActionResult Index()
+        {
+            Product product = new Product
+            {
+                Id = 1,
+                Name = "SSD 512GB",
+                Price = 2500
+            };
+            return View(product);
+        }
+    }
+}
